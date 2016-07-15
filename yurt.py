a9fbc0ebbf1480bad5e11439f98b3f12d3c434d3
jharriganASC <harrigan.joshua@gmail.com>
2016-07-15 09:18:49 -0400

    If and Then Functions

---
+++ yurt.py
@@ -0,0 +1,15 @@
+go Bronx = False
+goManhattan = False
+goQueens = False
+
+if goBronx:
+    print("You went to the Bronx")
+    
+elif goManhattan:
+    print("You went to Manhattan")
+    
+elif goQueens:
+    print("You went to Queens")
+        
+else:
+    print("You called Mommy")            
