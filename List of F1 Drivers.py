0e7e4500f1eb11584b0c070ad3ff1746b1a666b5
jharriganASC <harrigan.joshua@gmail.com>
2016-07-18 10:41:16 -0400

    List of Formula 1 Drivers Script (random project)

---
+++ List of F1 Drivers.py
@@ -0,0 +1,9 @@
+print("F1 Drivers")
+myList = ["Lewis Hamilton", "Valentino Rossi", "Nicki Lauda" "James Hunt"]
+
+print(len(myList))
+
+i = 0
+    while i < len(myList)
+    print(myList(1))
+    i = i + 1
